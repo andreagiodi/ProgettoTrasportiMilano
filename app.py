341e4aa6b8f702047a288d65ee4073778caea7db
andreagiodi <giodice.andrea@iisgalvanimi.edu.it>
2022-04-05 11:17:18 +0000

    commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_file, make_response, url_for, Res
 app = Flask(__name__)
 
 import io
+import pandas as pd
 import geopandas as gpd
 import contextily
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -11,10 +12,42 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
+quartieri = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/quartieri_milano.zip')
+area_sosta_car_sharing = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/area_sosta_car_sharing.zip')
+aree_velocita_lim = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/aree_velocita_lim.zip')
+bike_sosta = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/bike_sosta.zip')
+comuni = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/comuni.zip')
+#milano = comuni[comuni.NOME == 'Milano']
+comuni = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/comuni.zip')
+fermate_metro = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/fermate_metro.zip')
+fermate_superficie = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/fermate_superifcie.zip')
+fontanelle = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/Fontanelle.zip')
+parcheggi_pubblici = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/parcheggi_pubblici.zip')
+pedoni_ztl = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/pedoni_ztl.zip')
+percorsi_metro = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/percorsi_metro.zip')
+percorsi_superficie = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/percorsi_superficie.zip')
+piste_ciclabili = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/piste_ciclabili.zip')
+province = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/province.zip')
+quartieri_milano = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/quartieri_milano.zip')
+sosta_turistici = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/sosta_turistici.zip')
+stazioni_bikemi = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/stazioni_bikemi.zip')
+stazioni_milano = gpd.read_file('/workspace/ProgettoTrasportiMilano/filezip/stazioni_milano.csv')
+
+
+
 
 @app.route('/', methods=['GET'])
-def home():
-    return render_template('home.html')
+def index():
+    #ps = percorsi_superficie[['linea', 'nome']]                         #.set_index('nome')
+    #ps = ps.to_html(index=False)
+
+
+
+
+
+
+    
+    return render_template('index.html', table=ps)
 
 
 
