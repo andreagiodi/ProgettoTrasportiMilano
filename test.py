64f19b2cffe2feff5c7ac9e0243a583071833ea5
andreagiodi <giodice.andrea@iisgalvanimi.edu.it>
2022-05-04 12:09:13 +0000

    commit

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
 #https://www.codespeedy.com/how-to-pass-javascript-variables-to-python-in-flask/
 from flask import Flask, render_template, send_file, make_response, url_for, Response, request
-
+import json
 app = Flask(__name__)
-
-global value1
-value1 = ''
-global value2
 
 @app.route('/')
 def test():
     
-    print(value1)
-    return render_template('test1.html', value1=value1)
+    return render_template('test1.html')
 
 
 if __name__ == '__main__':
